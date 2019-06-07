82fd4a075d71bea545b9f08538e17adf7ba4bf66
Grzegorz <grzegorzgolan18@gmail.com>
2019-06-07 12:09:43 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int a;
         System.out.println("Wpisz wielkosc tablicy");
         a = scan.nextInt();
         int[] tab = new int[a];
-
+        System.out.println("podaj elementy tablicy");
         Method.uzup(tab);
 
         int[] tab2 = Method.odw(tab);
         System.out.println();
         for (int i = 0; i < tab2.length; i++) {
-
             System.out.print(tab2[i] + " ");
         }
-
     }
 }
--- src/Method.java
+++ src/Method.java
@@ -17,35 +17,17 @@ public class Method {
         for (int i = 0; i < tab.length; i++) {
             tab2[i] = tab[i];
         }
-
-
         for (int i = 0; i <= a - 1; i++) {
-
             tab2[i] = tab[b];
             b--;
-
         }
-
         return tab2;
     }
 
     public static int[] uzup(int[] tab) {
-        System.out.println("podaj elementy tablicy");
-        do {
-
             for (int i = 0; i < tab.length; i++) {
-                do {
                     tab[i] = scan.nextInt();
-
-                } while (tab == null);
-
-
             }
-            if (tab == null) {
-                System.out.println("tablica nie może być nullem");
-            }
-        } while (tab == null);
-
         return tab;
     }
 }
